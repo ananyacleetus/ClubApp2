e109a77fd99fa6fe749ffa7804fb26bcc7a631c8
Ananya Cleetus <ananyacleetus@gmail.com>
2015-03-16 02:44:59 -0400

    feed

--- platforms/ios/www/js/data.js
+++ platforms/ios/www/js/data.js
@@ -1,2 +1,5 @@
 var upd = {"SDC":"updsdc","IVC":"updivc", "MCC":"updmcc"}
-var cal = {"SDC":"calsdc","IVC":"calivc", "MCC":"calmcc"}
+var cal = {"SDC":"calsdc","IVC":"calivc", "MCC":"calmcc"} 
+
+
+var clubs = {"Swing Dancing Club":"sdc", "Invisible Children Club":"ivc", "Multicultural Club":"mcc"}
